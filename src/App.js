3ab97b3aaa87f0edb19af5b8c7efca89fed32f9c
paruljamwal <paruljamwal2000@gmail.com>
2022-11-25 10:57:16 +0530

    folder structur

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Folder from './component/Folder';
+import explorer from './data/folderData';
 
 function App() {
-  return (
+const [explorerData,setExplorerData]= useState(explorer);
+console.log(explorerData); 
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Folder explorer={explorerData} />
     </div>
   );
 }
---
+++ src/component/Folder.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Folder = ({explorer}) => {
+    console.log(explorer);
+  return (
+    <div>folder</div>
+  )
+}
+
+export default Folder;
